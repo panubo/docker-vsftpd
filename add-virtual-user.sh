a77eae39d3c27fe9f0810bae7cba19a20c5df02a
Tim Robinson <tim@panubo.com>
2017-07-29 13:11:44 +1000

    Fix code styling

--- add-virtual-user.sh
+++ add-virtual-user.sh
@@ -2,22 +2,22 @@
 
 set -e
 
-[ "$DEBUG" == 'true' ] && set -x
+[[ "${DEBUG}" == 'true' ]] && set -x
 
 DB=/etc/vsftpd/virtual-users.db
 
-if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
-    echo "Usage: $0 [-d] <user> <password>" >&2
-    echo >&2
-    echo "[ -d ] Delete the database first" >&2
-    exit 1
+if [[ "$#" -lt 2 || "$#" -gt 3 ]]; then
+  echo "Usage: $0 [-d] <user> <password>" >&2
+  echo >&2
+  echo "[ -d ] Delete the database first" >&2
+  exit 1
 fi
 
-if [ "$1" == "-d" ]; then
-    if [ -f $DB ]; then
-        rm $DB
-    fi
-    shift
+if [[ "${1}" == "-d" ]]; then
+  if [[ -f "${DB}" ]]; then
+    rm "${DB}"
+  fi
+  shift
 fi
 
-echo -e "$1\n$2" | db5.3_load -T -t hash $DB
+echo -e "${1}\n${2}" | db5.3_load -T -t hash "${DB}"
